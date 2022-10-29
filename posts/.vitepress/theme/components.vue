b3acd26e920e3c413018c2f798878bee5dc5cf1a
Smileslime47 <smile_slime_47@outlook.com>
2022-10-30 01:54:02 +0800

    delete proxy

--- posts/.vitepress/theme/components.vue
+++ posts/.vitepress/theme/components.vue
@@ -33,7 +33,6 @@ onMounted(() => {
       admin: ['Smileslime47'], // GitHub repository 所有者
       labels: ['Gitalk'], // GitHub issue 的标签
       createIssueManually: true, //如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
-      proxy: "https://vercel.younglina.top/github_access_token",
     })
     gitment.render('gitalk-page-container')
   }

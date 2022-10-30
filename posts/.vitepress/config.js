a557d11ffd3c2b8a5c105202de7a931265ec9f70
Smileslime47 <smile_slime_47@outlook.com>
2022-10-30 19:01:11 +0800

    add posts

--- posts/.vitepress/config.js
+++ posts/.vitepress/config.js
@@ -1,3 +1,4 @@
+//从外部引入nav和sidebar设置
 import myNav from './nav.js';
 import myBar from './sidebar.js';
 
@@ -6,10 +7,12 @@ import { withMermaid } from "vitepress-plugin-mermaid";
 import mathjax3 from 'markdown-it-mathjax3';
 import AutoNavPlugin from 'vitepress-auto-nav-sidebar';
 const customElements = ['mjx-container'];
+
+//自动生成侧边栏，在windows环境下会生成绝对路径导致运行后不可见，部署到github上显示正常
 const { nav, sidebar } = AutoNavPlugin({
   entry:'posts',
   ignoreFolders: ["node_modules", "assets", "public", ".vitepress", "code", ".obsidian", "utils"], // 需要排除的一些目录
-  ignoreFiles: ['个人简历'], // 需要排除的一些文件
+  ignoreFiles: ['index'], // 需要排除的一些文件
   dirPrefix: '目录：',
   filePrefix: '文件：',
   showNavIcon:false,
@@ -21,7 +24,7 @@ const { nav, sidebar } = AutoNavPlugin({
 
 export default withMermaid({
   markdown: {
-    //mathjax插件
+    //mathjax配置
     config: (md) => {
       md.use(mathjax3);
     },
@@ -29,34 +32,39 @@ export default withMermaid({
     lineNumbers: true,
   },
   
+  //mathjax3配置
   vue: {
     template: {
-      //mathjax3
       compilerOptions: {
         isCustomElement: (tag) => customElements.includes(tag),
       },
     },
   },
 
-  //metadata
-  title: '47Saikyo',
-  description: 'Just playing around.',
-  ignoreDeadLinks: true,
+  //config
+  title: '47Saikyo',                  //网站标题
+  description: 'Just playing around.',//网站描述，生成对应meta信息
+  //base:'',                          //如果网站没有部署在服务器的root目录下，如https://47saikyo.moe/myblog，则应当设置base:'/myblog/'
+  ignoreDeadLinks: true,              //忽视deadlink导致的构建错误
 
-  themeConfig: {
-    siteTitle: '47‘s Blog',
+  themeConfig: {                      //Vitepress的主题设置
+    siteTitle: '47‘s Blog',           //显示在导航栏左侧的网站标题
+    //侧边栏和导航栏，这里我用了自动生成的插件
     nav,
     sidebar,
 
+    //社交链接
     socialLinks: [
       { icon: 'github', link: 'https://github.com/Smileslime47/' },
     ],
 
+    //编辑链接
     editLink: {
       pattern: 'https://github.com/Smileslime47/Smileslime47.github.io/tree/main/posts/:path',
       text: 'Edit this page on GitHub'
     },
 
+    //测试docsearch
     algolia: { 
       appId: '8J64VVRP8K', 
       apiKey: 'a18e2f4cc5665f6602c5631fd868adfd', 

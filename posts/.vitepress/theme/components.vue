4f368dd9fbaa4428a590bf3c55f53f4ed54a7180
Smileslime47 <smile_slime_47@outlook.com>
2022-10-31 00:56:34 +0800

    add posts

--- posts/.vitepress/theme/components.vue
+++ posts/.vitepress/theme/components.vue
@@ -22,18 +22,18 @@ const members = [
 
 onMounted(() => {
   if(typeof window !==undefined){
-    var s_div = document.createElement('div');   // 创建节点
-    s_div.setAttribute("id", "gitalk-page-container");   // 设置id
-    document.querySelector('.content-container').appendChild(s_div);   // querySelector的节点可自己根据自己想加载的地方设置
-    var gitment = new Gitalk({
-      id: location.pathname, // 可选。默认为 location.href
-      owner: 'Smileslime47', // GitHub repository 所有者
-      repo: 'Smileslime47.github.io', // GitHub repository
-      clientID: '308b0c7a6bcdc200a34d', // 自己的clientID
-      clientSecret: 'd28ca59ffc98a45310830bad5d48c382a29d7667',// 自己的clientSecret
-      admin: ['Smileslime47'], // GitHub repository 所有者
-      labels: ['Gitalk'], // GitHub issue 的标签
-      createIssueManually: false, //如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
+    var s_div = document.createElement('div');  
+    s_div.setAttribute("id", "gitalk-page-container");   
+    document.querySelector('.content-container').appendChild(s_div);
+    const gitalk = new Gitalk({
+    id: location.pathname, 
+    owner: 'Smileslime47', 
+    repo: 'Smileslime47.github.io', 
+    clientID: '308b0c7a6bcdc200a34d', 
+    clientSecret: 'd28ca59ffc98a45310830bad5d48c382a29d7667',
+    admin: ['Smileslime47'], 
+    labels: ['Gitalk'], 
+    createIssueManually: false, 
     })
     gitment.render('gitalk-page-container')
   }
